012756e1d9bf366f860e16a460adcaf082217e39
kaminzhi <kamin@kaminzhi.com>
2025-05-15 17:39:41 +0800

    Initial Commit, basic build

---
+++ bot/main.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name} ({bot.user.id})")
+    await bot.tree.sync()
+
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
