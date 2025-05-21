1e65d9aff3d32747a7d2aac9db6a06b7396909e2
kaminzhi <kamin@kaminzhi.com>
2025-05-21 11:46:32 +0800

    feat: bot status

--- bot/main.py
+++ bot/main.py
@@ -11,13 +11,43 @@ logger = logging.getLogger(__name__)
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-SERVER_GUILD_ID = os.getenv("SERVER_GUILD_ID")
+SERVER_GUILD_ID = int(os.getenv("SERVER_GUILD_ID", ""))
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+BOT_STATUS = os.getenv("BOT_STATUS", "online")
+ACTIVITY_TYPE = os.getenv("BOT_ACTIVITY_TYPE", None)
+ACTIVITY_TEXT = os.getenv("BOT_ACTIVITY_TEXT")
+ACTIVITY_URL = os.getenv("BOT_ACTIVITY_URL", None)
+
+bot = commands.Bot(
+    command_prefix=f"{os.getenv('command_prefix', '!')}", intents=intents
+)
+
+
+def get_activity(activity_type, activity_text, activity_url=None):
+    if activity_type == "custom":
+        return discord.CustomActivity(name=activity_text)
+    elif activity_type == "streaming":
+        return discord.Streaming(
+            name=activity_text, url=activity_url or "https://twitch.tv/placeholder"
+        )
+    elif activity_type == "listening":
+        return discord.Activity(type=discord.ActivityType.listening, name=activity_text)
+    elif activity_type == "watching":
+        return discord.Activity(type=discord.ActivityType.watching, name=activity_text)
+    else:
+        return discord.Game(name=activity_text or "啟動中...")
+
+
+status_map = {
+    "online": discord.Status.online,
+    "idle": discord.Status.idle,
+    "dnd": discord.Status.dnd,
+    "invisible": discord.Status.invisible,
+}
 
 
 @bot.event
@@ -29,9 +59,10 @@ async def on_ready():
     except Exception as e:
         logging.error(f"❌ MongoDB Connection Fail: {e}")
 
-    # await bot.load_extension("bot.cogs.messaging")
+    # Load your cogs (extensions) here.
+    # await bot.load_extension("bot.cogs.messaging") # Uncomment if you need this
     await bot.load_extension("bot.cogs.link_fixer")
-    # await bot.load_extension("bot.cogs.welcome")
+    # await bot.load_extension("bot.cogs.welcome") # Uncomment if you need this
     await bot.load_extension("bot.cogs.ping")
     await bot.load_extension("bot.cogs.linkfix_settings")
 
@@ -56,6 +87,13 @@ async def on_ready():
             logger.info(f"✅ 已進行全域指令同步 {len(commands)} 個指令。")
         except Exception as e:
             logger.error(f"❌ 全域指令同步時發生錯誤：{e}")
+
+    activity = get_activity(ACTIVITY_TYPE, ACTIVITY_TEXT, ACTIVITY_URL)
+    status = status_map.get(BOT_STATUS)
+    await bot.change_presence(status=status, activity=activity)
+    logger.info(
+        f"✅ 狀態已設置為 {BOT_STATUS}，類型為 {ACTIVITY_TYPE}, 文字為 {ACTIVITY_TEXT}。"
+    )
 
 
 if __name__ == "__main__":
